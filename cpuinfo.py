665f5474898c3222ed600b5449995f2145ad833c
Matthew Brennan Jones <mattjones@workhorsy.org>
2013-11-07 11:38:31 -0800

    Added support for getting extended cpu features and the processor brand string.

--- cpuinfo.py
+++ cpuinfo.py
@@ -45,6 +45,8 @@ main:
 	mov ax, bx
 	ret
 '''
+
+import os
 import platform
 import ctypes
 # FIXME: Windows is missing valloc. Use VirtualAlloc instead:
@@ -146,7 +148,6 @@ def get_info():
 		b"\x0f\xa2"         # cpuid
 		b"\xC3"             # ret
 	)
-	print('eax', eax)
 
 	# Get the CPU info
 	stepping = (eax >> 0) & 0xF # 4 bits
@@ -165,8 +166,18 @@ def get_info():
 		'extended_family' : extended_family
 	}
 
+def get_max_extension_support():
+	# Check for extension support
+	max_extension_support = run_asm(
+		b"\xB8\x00\x00\x00\x80" # mov ax,0x80000000
+		b"\x0f\xa2"             # cpuid
+		b"\xC3"                 # ret
+	)
+
+	return max_extension_support
+
 # http://en.wikipedia.org/wiki/CPUID#EAX.3D1:_Processor_Info_and_Feature_Bits
-def get_flags():
+def get_flags(max_extension_support):
 	# EDX
 	edx = run_asm(
 		_one_eax(),
@@ -222,7 +233,7 @@ def get_flags():
 		'pclmulqdq' : is_bit_set(ecx, 1),
 		'dtes64' : is_bit_set(ecx, 2),
 		'monitor' : is_bit_set(ecx, 3),
-		'ds_cpi' : is_bit_set(ecx, 4),
+		'ds_cpl' : is_bit_set(ecx, 4),
 		'vmx' : is_bit_set(ecx, 5),
 		'smx' : is_bit_set(ecx, 6),
 		'est' : is_bit_set(ecx, 7),
@@ -254,12 +265,171 @@ def get_flags():
 
 	# Get a list of only the flags that are true
 	flags = [k for k, v in flags.items() if v]
-	flags.sort()
 
+	# Get the Extended CPU flags
+	extended_flags = {}
+	if max_extension_support >= 0x80000001:
+		# EDX
+		edx = run_asm(
+			b"\xB8\x01\x00\x00\x80" # mov ax,0x80000001
+			b"\x0f\xa2"         # cpuid
+			b"\x89\xD0"         # mov ax,dx
+			b"\xC3"             # ret
+		)
+
+		# ECX
+		ecx = run_asm(
+			b"\xB8\x01\x00\x00\x80" # mov ax,0x80000001
+			b"\x0f\xa2"         # cpuid
+			b"\x89\xC8"         # mov ax,cx
+			b"\xC3"             # ret
+		)
+
+		# Get the extended CPU flags
+		extended_flags = {
+			'fpu' : is_bit_set(edx, 0),
+			'vme' : is_bit_set(edx, 1),
+			'de' : is_bit_set(edx, 2),
+			'pse' : is_bit_set(edx, 3),
+			'tsc' : is_bit_set(edx, 4),
+			'msr' : is_bit_set(edx, 5),
+			'pae' : is_bit_set(edx, 6),
+			'mce' : is_bit_set(edx, 7),
+			'cx8' : is_bit_set(edx, 8),
+			'apic' : is_bit_set(edx, 9),
+			#'reserved' : is_bit_set(edx, 10),
+			'syscall' : is_bit_set(edx, 11),
+			'mtrr' : is_bit_set(edx, 12),
+			'pge' : is_bit_set(edx, 13),
+			'mca' : is_bit_set(edx, 14),
+			'cmov' : is_bit_set(edx, 15),
+			'pat' : is_bit_set(edx, 16),
+			'pse36' : is_bit_set(edx, 17),
+			#'reserved' : is_bit_set(edx, 18),
+			'mp' : is_bit_set(edx, 19),
+			'nx' : is_bit_set(edx, 20),
+			#'reserved' : is_bit_set(edx, 21),
+			'mmxext' : is_bit_set(edx, 22),
+			'mmx' : is_bit_set(edx, 23),
+			'fxsr' : is_bit_set(edx, 24),
+			'fxsr_opt' : is_bit_set(edx, 25),
+			'pdpe1gp' : is_bit_set(edx, 26),
+			'rdtscp' : is_bit_set(edx, 27),
+			#'reserved' : is_bit_set(edx, 28),
+			'lm' : is_bit_set(edx, 29),
+			'3dnowext' : is_bit_set(edx, 30),
+			'3dnow' : is_bit_set(edx, 31),
+
+			'lahf_lm' : is_bit_set(ecx, 0),
+			'cmp_legacy' : is_bit_set(ecx, 1),
+			'svm' : is_bit_set(ecx, 2),
+			'extapic' : is_bit_set(ecx, 3),
+			'cr8_legacy' : is_bit_set(ecx, 4),
+			'abm' : is_bit_set(ecx, 5),
+			'sse4a' : is_bit_set(ecx, 6),
+			'misalignsse' : is_bit_set(ecx, 7),
+			'3dnowprefetch' : is_bit_set(ecx, 8),
+			'osvw' : is_bit_set(ecx, 9),
+			'ibs' : is_bit_set(ecx, 10),
+			'xop' : is_bit_set(ecx, 11),
+			'skinit' : is_bit_set(ecx, 12),
+			'wdt' : is_bit_set(ecx, 13),
+			#'reserved' : is_bit_set(ecx, 14),
+			'lwp' : is_bit_set(ecx, 15),
+			'fma4' : is_bit_set(ecx, 16),
+			'tce' : is_bit_set(ecx, 17),
+			#'reserved' : is_bit_set(ecx, 18),
+			'nodeid_msr' : is_bit_set(ecx, 19),
+			#'reserved' : is_bit_set(ecx, 20),
+			'tbm' : is_bit_set(ecx, 21),
+			'topoext' : is_bit_set(ecx, 22),
+			'perfctr_core' : is_bit_set(ecx, 23),
+			'perfctr_nb' : is_bit_set(ecx, 24),
+			#'reserved' : is_bit_set(ecx, 25),
+			#'reserved' : is_bit_set(ecx, 26),
+			#'reserved' : is_bit_set(ecx, 27),
+			#'reserved' : is_bit_set(ecx, 28),
+			#'reserved' : is_bit_set(ecx, 29),
+			#'reserved' : is_bit_set(ecx, 30),
+			#'reserved' : is_bit_set(ecx, 31)
+		}
+
+	# Get a list of only the flags that are true
+	extended_flags = [k for k, v in extended_flags.items() if v]
+	flags += extended_flags
+
+	flags.sort()
 	return flags
 
+def processor_brand(max_extension_support):
+	processor_brand = ""
 
+	# Processor brand string
+	if max_extension_support >= 0x80000004:
+		instructions = [
+			b"\xB8\x02\x00\x00\x80", # mov ax,0x80000002
+			b"\xB8\x03\x00\x00\x80", # mov ax,0x80000003
+			b"\xB8\x04\x00\x00\x80"  # mov ax,0x80000004
+		]
+		for instruction in instructions:
+			# EAX
+			eax = run_asm(
+				instruction,  # mov ax,0x8000000?
+				b"\x0f\xa2"   # cpuid
+				b"\x89\xC0"   # mov ax,ax
+				b"\xC3"       # ret
+			)
+
+			# EBX
+			ebx = run_asm(
+				instruction,  # mov ax,0x8000000?
+				b"\x0f\xa2"   # cpuid
+				b"\x89\xD8"   # mov ax,bx
+				b"\xC3"       # ret
+			)
+
+			# ECX
+			ecx = run_asm(
+				instruction,  # mov ax,0x8000000?
+				b"\x0f\xa2"   # cpuid
+				b"\x89\xC8"   # mov ax,cx
+				b"\xC3"       # ret
+			)
+
+			# EDX
+			edx = run_asm(
+				instruction,  # mov ax,0x8000000?
+				b"\x0f\xa2"   # cpuid
+				b"\x89\xD0"   # mov ax,dx
+				b"\xC3"       # ret
+			)
+
+			# Combine each of the 4 bytes in each register into the string
+			for reg in [eax, ebx, ecx, edx]:
+				for n in [0, 8, 16, 24]:
+					processor_brand += chr((reg >> n) & 0xFF)
+
+	return processor_brand[:-1]
+
+'''
+real_flags = os.popen('cat /proc/cpuinfo').read().split('flags')[1].split('\n')[0].split()
+real_flags.sort()
+flags = get_flags()
+print('Flags not in py-cpuinfo, but in /proc/cpuinfo ----------------------')
+for flag in real_flags:
+	if not flag in flags:
+		print(flag)
+
+print('Flags not in /proc/cpuinfo, but in py-cpuinfo ------------------')
+for flag in flags:
+	if not flag in real_flags:
+		print(flag)
+
+exit()
+'''
+max_extension_support = get_max_extension_support()
 print('vendor_id', get_vendor_id())
+print('processor_brand', processor_brand(max_extension_support))
 info = get_info()
 print('stepping', info['stepping'])
 print('model', info['model'])
@@ -267,6 +437,6 @@ print('family', info['family'])
 print('processor_type', info['processor_type'])
 print('extended_model', info['extended_model'])
 print('extended_family', info['extended_family'])
-print(get_flags())
+print(get_flags(max_extension_support))
 
 
