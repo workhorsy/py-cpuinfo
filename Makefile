f4f72516dd0f8300ecc7b47975ed224891c88a78
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2015-02-11 16:47:53 -0800

    Release 0.1.4

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 
-VERSION = 0.1.3
+VERSION = 0.1.4
 
 all:
 	@echo build: Builds the python source dist package
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(os.getcwd(), 'README.rst'), 'r') as f:
 
 setup(
     name = "py-cpuinfo",
-    version = "0.1.3",
+    version = "0.1.4",
     author = "Matthew Brennan Jones",
     author_email = "matthew.brennan.jones@gmail.com",
     description = "Get CPU info with pure Python 2 & 3",
