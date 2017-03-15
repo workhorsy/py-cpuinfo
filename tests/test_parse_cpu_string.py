f42b595f42b1fa473c74aa24d231d6f036f0a292
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-03-15 05:15:31 -0700

    Added test to make sure it works with brand strings with no @.

--- tests/test_parse_cpu_string.py
+++ tests/test_parse_cpu_string.py
@@ -48,6 +48,17 @@ class TestParseCPUString(unittest.TestCase):
 		self.assertEqual(None, model)
 		self.assertEqual(None, family)
 
+		# NOTE: No @ symbol
+		processor_brand, hz_actual, scale, vendor_id, stepping, model, family = \
+		cpuinfo._parse_cpu_string("Intel(R) Pentium(R) D CPU 3.20GHz")
+		self.assertEqual("Intel(R) Pentium(R) D CPU 3.20GHz", processor_brand)
+		self.assertEqual('3.2', hz_actual)
+		self.assertEqual(9, scale)
+		self.assertEqual(None, vendor_id)
+		self.assertEqual(None, stepping)
+		self.assertEqual(None, model)
+		self.assertEqual(None, family)
+
 	def test_to_friendly_hz(self):
 		scale, hz_brand = cpuinfo._get_hz_string_from_brand('Intel(R) Pentium(R) CPU G640 @ 2.80GHz')
 		self.assertEqual(9, scale)
@@ -59,6 +70,11 @@ class TestParseCPUString(unittest.TestCase):
 		self.assertEqual('1.2', hz_brand)
 		self.assertEqual('1.2000 MHz', to_friendly_hz(hz_brand, scale))
 
+		scale, hz_brand = cpuinfo._get_hz_string_from_brand('Intel(R) Pentium(R) D CPU 3.20GHz')
+		self.assertEqual(9, scale)
+		self.assertEqual('3.2', hz_brand)
+		self.assertEqual('3.2000 GHz', to_friendly_hz(hz_brand, scale))
+
 	def test_to_raw_hz(self):
 		scale, hz_brand = cpuinfo._get_hz_string_from_brand('Intel(R) Pentium(R) CPU G640 @ 2.80GHz')
 		self.assertEqual(9, scale)
@@ -69,3 +85,9 @@ class TestParseCPUString(unittest.TestCase):
 		self.assertEqual(6, scale)
 		self.assertEqual('1.2', hz_brand)
 		self.assertEqual((1200000, 0), to_raw_hz(hz_brand, scale))
+
+		# NOTE: No @ symbol
+		scale, hz_brand = cpuinfo._get_hz_string_from_brand('Intel(R) Pentium(R) D CPU 3.20GHz')
+		self.assertEqual(9, scale)
+		self.assertEqual('3.2', hz_brand)
+		self.assertEqual((3200000000, 0), to_raw_hz(hz_brand, scale))
