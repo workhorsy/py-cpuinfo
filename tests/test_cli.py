66293e67d8d604eea273431c0243ee1be2679cb6
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2019-02-28 16:23:50 -0800

    Added unit test for default cli output.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -23,3 +23,14 @@ class TestCLI(unittest.TestCase):
 		info = json.loads(output)
 
 		self.assertEqual(list(cpuinfo.CPUINFO_VERSION), info['cpuinfo_version'])
+
+	def test_default(self):
+		command = [sys.executable, 'cpuinfo/cpuinfo.py']
+		p1 = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+		output = p1.communicate()[0]
+
+		self.assertEqual(0, p1.returncode)
+
+		version = output.split('Cpuinfo Version: ')[1].split('\n')[0].strip()
+
+		self.assertEqual(str(cpuinfo.CPUINFO_VERSION), version)
