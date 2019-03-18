78d0bc7617ca2567f038fe3cdcdbf65470d2984d
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2019-03-17 23:45:44 -0700

    Removed test files for no main.

--- test_main.py
+++
@@ -1,9 +0,0 @@
-
-
-if __name__ == '__main__':
-	from multiprocessing import freeze_support
-	from cpuinfo import get_cpu_info
-
-	freeze_support()
-	get_cpu_info()
-	print('This should only be printed once per test')
