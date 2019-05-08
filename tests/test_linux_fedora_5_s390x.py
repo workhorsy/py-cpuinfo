d8a9c9b095be55d5ceebf42497f099039fe8c348
Than Ngo <than@redhat.com>
2019-05-08 20:09:20 +0200

    use _get_cpu_info_internal(), fix AssertionError

--- tests/test_linux_fedora_5_s390x.py
+++ tests/test_linux_fedora_5_s390x.py
@@ -397,7 +397,7 @@ class TestLinuxFedora_5_s390x(unittest.TestCase):
 		self.assertEqual(0, len(cpuinfo._get_cpu_info_from_ibm_pa_features()))
 		self.assertEqual(0, len(cpuinfo._get_cpu_info_from_sysinfo()))
 		self.assertEqual(0, len(cpuinfo._get_cpu_info_from_cpuid()))
-		self.assertEqual(17, len(cpuinfo.get_cpu_info()))
+		self.assertEqual(17, len(cpuinfo._get_cpu_info_internal()))
 
 	def test_get_cpu_info_from_lscpu(self):
 		info = cpuinfo._get_cpu_info_from_lscpu()
