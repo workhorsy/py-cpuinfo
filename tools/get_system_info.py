af64061f0f3385bd58af65ec3c15a92d9088f237
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-02-01 11:00:49 -0800

    Updated other copyright dates

--- tools/get_system_info.py
+++ tools/get_system_info.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-# Copyright (c) 2014-2020 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
+# Copyright (c) 2014-2021 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 # Py-cpuinfo gets CPU info with pure Python 2 & 3
 # It uses the MIT License
 # It is hosted at: https://github.com/workhorsy/py-cpuinfo
