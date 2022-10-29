fba5f5e68e3ef28c7d3db5d5fed110fa4df19544
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2022-10-29 11:44:14 +0200

    Migrate setup.py → pyproject.toml
    See PEP 621 and PEP 518.
    
    Keep setup.py for legacy installs.
    The proper way to install is now: pip install

--- setup.py
+++ setup.py
@@ -1,33 +1,10 @@
+#!/usr/bin/env python
 # Copyright (c) 2014-2022 Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
 # Py-cpuinfo gets CPU info with pure Python
 # It uses the MIT License
 # It is hosted at: https://github.com/workhorsy/py-cpuinfo
 
-import os
 from setuptools import setup
 
-with open(os.path.join(os.getcwd(), 'README.rst')) as f:
-    readme_content = f.read()
-
-setup(
-    name = "py-cpuinfo",
-    version = "9.0.0",
-    author = "Matthew Brennan Jones",
-    author_email = "matthew.brennan.jones@gmail.com",
-    description = "Get CPU info with pure Python",
-    long_description=readme_content,
-    python_requires='>=3',
-    license = "MIT",
-    url = "https://github.com/workhorsy/py-cpuinfo",
-    packages=['cpuinfo'],
-    test_suite="test_suite",
-    entry_points = {
-        'console_scripts': ['cpuinfo = cpuinfo:main'],
-    },
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3"
-    ],
-)
+if __name__ == "__main__":
+    setup()
